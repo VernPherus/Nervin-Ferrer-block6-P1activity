1ab466e57b25216bfbea2802327122e9a104dae3
VernPherus <vernpherus@gmail.com>
2023-12-05 14:03:37 +0800

    added button functionality

--- app/src/main/java/com/ferrer/nervin/section/p1/quiz/MainActivity.kt
+++ app/src/main/java/com/ferrer/nervin/section/p1/quiz/MainActivity.kt
@@ -3,10 +3,21 @@ package com.ferrer.nervin.section.p1.quiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ferrer.nervin.section.p1.quiz.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        val msg = "Button_pressed"
+
+        binding.button.setOnClickListener {
+            binding.textView.text = msg;
+        }
+
+        setContentView(binding.root)
     }
 }
